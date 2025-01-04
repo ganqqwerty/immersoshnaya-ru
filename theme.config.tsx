eb008d392b871917a588927f2883cb21b3c999d8
Yuri Katkov <katkov.juriy@gmail.com>
2025-01-04 21:58:43 +0100

    cleanup, adding menu

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Иммерсошная</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
   chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
+    link: 'https://t.me/immersionjp',
+    icon: './icons/telegram-icon.svg',
   },
 }
 
